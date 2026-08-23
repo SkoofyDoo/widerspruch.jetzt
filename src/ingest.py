ba056adef74863cf1f25fa3092b7ec86a5219381
EVGENY KVEST <evgenykvest@gmail.com>
2026-08-23 15:12:37 +0200

    Full Ingest Pipeline

--- src/ingest.py
+++ src/ingest.py
@@ -1,16 +1,24 @@
 import os
 
 import chromadb 
+import re
+from pathlib import Path
 from dotenv import load_dotenv
 
-from langchain_ollama import OllamaEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import DirectoryLoader, TextLoader
 
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_core.documents import Document
 from chromadb.utils.embedding_functions import create_langchain_embedding
 from chromadb.config import Settings
 
 load_dotenv()
+
+
+
+print("=" * 70)
+print("RAG WITH LANGCHAIN")
+print("=" * 70)
 
 
 #========================================
@@ -28,7 +36,7 @@ embeddings = OllamaEmbeddings(
 #TODO: Search for new approaches
 loader = DirectoryLoader(
     path = "../data/cleaned/", 
-    glob = "**/*.txt",
+    glob = "sgb*.txt",
     loader_cls = TextLoader,
     loader_kwargs = {"encoding": "utf-8"}
     )
@@ -37,72 +45,155 @@ loader = DirectoryLoader(
 
 documents = loader.load()
 
-print(f"Anzahl der Dokumente: {len(documents)}")
+print(f"[TEXT LOADER]: Anzahl der Dokumente: {len(documents)}")
+
+
+# ================================================
+# METADATA AUS DEM TEXT NAME, GESETZT, PARAGRAPH
+# ================================================
+def meta_from_source(doc: Document) -> dict:
+    source = doc.metadata["source"]
+    source_file = Path(source).name
+    name = source_file.lower()
+
+    if name.startswith("sgb2__"):
+        law = "SGB II"
+        m = re.search(r"^sgb2__(\d+[a-z]?)_", source_file, re.I)
+    elif name.startswith("sgbx__"):
+        law = "SGB X"
+        m = re.search(r"__SGBX_(\d+[a-z]?)_", source_file, re.I)
+    else:
+        law = "UNKNOWN"
+        m = None
+
+    paragraph = f"§ {m.group(1)}" if m else ""
+    if not paragraph and text:
+        m2 = re.search(r"§\s*(\d+[a-z]?)", doc.page_content, re.I)
+        if m2:
+            paragraph = f"§ {m2.group(1)}"
+
+    return {
+        "source": source,
+        "source_file": source_file,
+        "law": law,
+        "paragraph": paragraph,
+    }
+
+
+# ================================================
+# CHUNKING (EIN PARAGRAPH - EIN CHUNK)
+# ================================================
+chunks = []
+for doc in documents:
+    
+    text = doc.page_content.strip()
+    meta = meta_from_source(doc)
+    # TODO: Anzahl chars anpassen
+    if len(text) <= 2000:
+        chunks.append(Document(page_content = text, metadata = meta))
+        continue
+
+    parts = re.split(r"\n(?=\(\d+[a-z]?\))", text)
+    header = parts[0].strip()
+    body = [p.strip() for p in parts[1:] if p.strip()]
+
+    if not body:
+        chunks.append(Document(page_content = text, metadata = meta))
+        continue
+
+    for p in body:
+        content = f"{header}\n{p}" if header else p
+        chunks.append(Document(page_content = content, metadata = meta))
+
+print(f"[CHUNKING]: Länge der Chunks {len(chunks)}")
+# print(chunks[0].page_content)
+# print(chunks[0].metadata)
+# print("-------")
+# print(chunks[1].page_content)
+# print(chunks[1].metadata)
+
+# TEST
+# chunks = chunks[:10]
+
+# ================================================
+# METADATA AUS FÜR CHROMA
+# ================================================
+
+ids = [f"{c.metadata['source_file']}::{i}" for i, c in enumerate(chunks)]
+texts = [c.page_content for c in chunks]
+metas = [{
+    "source_file": c.metadata["source_file"],
+    "law": c.metadata["law"],
+    "paragraph": c.metadata["paragraph"],
+    }
+    for c in chunks
+]
+
 
 # ========================================
-# TEXT SPLITTER
+# CHROMA
 # ========================================
+os.makedirs("../data/chroma/", exist_ok=True)
+print("[CHROMA]: Connection..")
 
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 1200,
-    chunk_overlap = 150,
-    separators = ["\n\n", "\n(", "\n", "\n1", "\n2", ". ", " "]
+
+
+client = chromadb.PersistentClient(
+    path="../data/chroma",
+    settings=Settings(anonymized_telemetry=False)
+    )
+
+print(f"[CHROMA]: Connected {client}")
+
+chroma_ef = create_langchain_embedding(embeddings)
+
+
+try:
+    client.delete_collection("wdjetzt")
+except Exception:
+    pass
+
+print("[CHROMA]: Collection gelöscht")
+
+try:
+    collection = client.get_or_create_collection (
+        name = "wdjetzt",
+        embedding_function = chroma_ef,
+        metadata = {"hnsw:space": "cosine"}      
+    )
+except Exception as e:
+    print("[CHROMA]: Collection konnte nicht generiert werden" + e)
+    raise  
+
+print(f"[CHROMA]: {collection}, Länge: {collection.count()}")
+
+collection.add(ids = ids, documents = texts, metadatas = metas)
+
+print(f"[CHROMA]: Anzahl: {collection.count()} items wurden hinzugefügt")
+
+print(collection.count() == len(chunks))
+
+
+query =  (
+    "Instruct: Given a question about German social law (SGB II / SGB X), "
+    "retrieve the most relevant legal paragraph.\n"
+    "Query: Wann ist eine Arbeit unzumutbar?"
 )
 
-chunks = splitter.split_documents(documents)
+result = collection.query(
+    query_texts = [query], 
+    n_results = 3
+)
 
-# TODO: REPAIR CHUNKING
-print(repr(chunks[0].page_content))
-print(f"CHUNK 2: {repr(chunks[1].page_content)}")
-print("---")
-
-print(f"Anzahl Chunks: {len(chunks)}")
-
-# ========================================
-# VECRORE_STORE
-# ========================================
-# os.makedirs("../data/chroma", exist_ok=True)
-# client = chromadb.PersistentClient(
-#     path="..data/chroma",
-#     settings=Settings(anonymized_telemetry=False)
-#     )
-
-# chroma_ef = create_langchain_embedding(embeddings)
-
-# try:
-#     client.delete_collection("wdjetzt")
-# except Exception:
-#     pass
-
-# print(f"[CHROMA]: Collection gelöscht")
-
-# collection = client.create_collection (
-#     name = "wdjetzt",
-#     embedding_function = chroma_ef,
-    
-# )
-# print(f"[CHROMA]: {collection}, Länge: {collection.count()}")
-
-# for i, d in enumearte:
-#     collection.add(ids = d.metadata["source"] + "::" + str(i) , 
-#                documents = d.page_content, 
-#                metadatas = [{"source" : d.metadata["source"]}]
-#                )
-
-# print(f"Anzahl: {collection.count()}")
-
-# print(d.metadata["source"])
-
-
-# Chroma.from_documents(
-#     documents, 
-#     embeddings, 
-#     persist_directory = "data/chroma", 
-#     collection_name = "wdjetzt_embeds")
-
-# ========================================
-# 3
-# =======================================
+for doc, meta, dist in zip(
+    result["documents"][0],
+    result["metadatas"][0],
+    result["distances"][0],
+):
+    print(dist, meta)
+    print(doc[:400])
+    print("---")
+                 
 
 if __name__ == "__main__":
     print("Well Done")
